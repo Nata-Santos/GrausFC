6e307345a559c8827d9460595ca2c59c02e705ce
Natã dos Santos Silva <78987318+NatanSantos155@users.noreply.github.com>
2021-03-13 01:05:50 -0300

    atividade GrausFC

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GrausFC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string entradaF;
+            double f;
+            double c;
+            double resultado;
+
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("Digite uma temperatura em °F");
+            Console.WriteLine("---------------------");
+            Console.Write("°F:");
+            entradaF = Console.ReadLine();
+
+            f = Convert.ToDouble(entradaF);
+            c = f - 32;
+            resultado = c / 1.8;
+
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"\n{f}°F equivalem a {resultado:N3}°C");
+            Console.ResetColor();
+
+
+
+
+
+        }
+    }
+}
